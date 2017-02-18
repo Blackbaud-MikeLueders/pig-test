f9ec6bee6eef8016a746a1c7fc8f4fa2c2580b77
Mike Lueders <Mike.Lueders@blackbaud.com>
2017-02-18 10:17:51 -0600

    add basic test

---
+++ src/test/groovy/com/blackbaud/pigtest/PigTestSpec.groovy
@@ -0,0 +1,11 @@
+package com.blackbaud.pigtest
+
+import spock.lang.Specification
+
+class PigTestSpec extends Specification {
+
+    def "should have a basic test" () {
+        expect:
+        true
+     }
+}
