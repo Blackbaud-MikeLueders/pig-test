eff0bc6222867ca15784a9dea87d638071791ead
Mike Lueders <Mike.Lueders@blackbaud.com>
2017-02-20 09:20:48 -0600

    checkpoint

--- src/main/resources/diff_groups.pig
+++ src/main/resources/diff_groups.pig
@@ -1,6 +1,6 @@
 -- Clear Outputs
-rmf $OUTPUT/out_members_to_remove.gz
-rmf $OUTPUT/out_members_to_add.gz
+rmf $OUTPUT/unmerged_members_to_remove.gz
+rmf $OUTPUT/unmerged_members_to_add.gz
 rmf $OUTPUT/members_to_remove.gz
 rmf $OUTPUT/members_to_add.gz
 
@@ -12,14 +12,14 @@ target = LOAD '$INPUT/target_group.gz' USING PigStorage('\n') AS target_member: 
 combined = JOIN source BY source_member FULL OUTER, target BY target_member;
 
 -- Output Data
-SPLIT combined INTO member_to_remove IF target_member IS NULL,
-                    member_to_add IF source_member IS NULL;
+SPLIT combined INTO member_to_add IF source_member IS NULL,
+                    member_to_remove IF target_member IS NULL;
 
-members_to_remove = FOREACH member_to_remove GENERATE source_member;
 members_to_add = FOREACH member_to_add GENERATE target_member;
+members_to_remove = FOREACH member_to_remove GENERATE source_member;
 
-STORE members_to_remove INTO '$OUTPUT/out_members_to_remove.gz' USING PigStorage();
-STORE members_to_add INTO '$OUTPUT/out_members_to_add.gz' USING PigStorage();
+STORE members_to_add INTO '$OUTPUT/unmerged_members_to_add.gz' USING PigStorage();
+STORE members_to_remove INTO '$OUTPUT/unmerged_members_to_remove.gz' USING PigStorage();
 
-fs -getmerge $OUTPUT/out_members_to_remove.gz $OUTPUT/members_to_remove.gz
-fs -getmerge $OUTPUT/out_members_to_add.gz $OUTPUT/members_to_add.gz
+fs -getmerge $OUTPUT/unmerged_members_to_add.gz $OUTPUT/members_to_add.gz
+fs -getmerge $OUTPUT/unmerged_members_to_remove.gz $OUTPUT/members_to_remove.gz
--- src/test/groovy/com/blackbaud/pigtest/PigTestSpec.groovy
+++ src/test/groovy/com/blackbaud/pigtest/PigTestSpec.groovy
@@ -1,72 +1,46 @@
 package com.blackbaud.pigtest
 
 import com.blackbaud.exec.ExecutionResult
-import com.blackbaud.pig.PigLocalExecutor
+import com.blackbaud.exec.pig.PigLocalExecutor
 import com.blackbaud.testsupport.RandomGenerator
 import spock.lang.Specification
 
-import java.util.zip.GZIPOutputStream
+import java.util.stream.IntStream
+import java.util.stream.Stream
 
 class PigTestSpec extends Specification {
 
     RandomGenerator aRandom = new RandomGenerator()
-    File tmpDir
-    File sourceFile
-    File targetFile
+    PigLocalExecutor executor = new PigLocalExecutor()
+            .hadoopBuildDir(new File("/Users/mike.lueders/IdeaProjects/Blackbaud/pig-test/build/hadoop/"))
     File pigScript
 
     def setup() {
-        tmpDir = File.createTempDir()
-        sourceFile = new File(tmpDir, "source_group.gz")
-        targetFile = new File(tmpDir, "target_group.gz")
         pigScript = new File(getClass().getClassLoader().getResource("diff_groups.pig").file)
-    }
-
-    def cleanup() {
-        tmpDir.deleteDir()
     }
 
     def "should diff files"() {
         given:
-        List<Integer> sourceIds = createFileWithMaxIds(sourceFile, 1000)
-        List<Integer> targetIds = createFileWithMaxIds(targetFile, 1000)
+        int maxGroupSize = 1000
+        List<Integer> sourceIds = []
+        List<Integer> targetIds = []
 
         when:
-        PigLocalExecutor executor = new PigLocalExecutor()
-                .hadoopBuildDir(new File("/Users/mike.lueders/IdeaProjects/Blackbaud/pig-test/build/hadoop/"))
-                .pigScript(pigScript)
-                .inputFiles(sourceFile, targetFile)
-        ExecutionResult result = executor.execute()
+        ExecutionResult result = executor.pigScript(pigScript)
+                .gzipInputFile("source_group.gz", createIdStream(maxGroupSize, sourceIds))
+                .gzipInputFile("target_group.gz", createIdStream(maxGroupSize, targetIds))
+                .execute()
 
         then:
         assert result.exitCode == 0
-
-        and:
         assertExpectedResults(executor, sourceIds, targetIds)
     }
 
-    private List<Integer> createFileWithMaxIds(File file, int maxId) {
-        List<Integer> idList = []
-        file.withOutputStream { OutputStream os ->
-            GZIPOutputStream gzipOs = new GZIPOutputStream(os);
-
-            StringBuilder builder = new StringBuilder()
-            for (int i = 0; i < maxId; i++) {
-                if (aRandom.coinFlip()) {
-                    idList << i
-                    builder.append(i).append('\n')
-                }
-                if (builder.size() > 100000) {
-                    gzipOs.write(builder.toString().bytes)
-                    builder.setLength(0)
-                }
-            }
-            if (builder.size() > 0) {
-                gzipOs.write(builder.toString().bytes)
-            }
-            gzipOs.close()
-        }
-        idList
+    private Stream createIdStream(int maxId, List idList) {
+        IntStream.range(0, maxId)
+                .filter({ aRandom.coinFlip() })
+                .peek({ i -> idList << i })
+                .boxed()
     }
 
     private void assertExpectedResults(PigLocalExecutor executor, List<Integer> sourceIds, List<Integer> targetIds) {
